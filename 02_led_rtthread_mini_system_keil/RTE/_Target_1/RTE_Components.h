632f7bd8860258fd14ec10f206d3d1db88304218
supperthomas <78900636@qq.com>
2020-11-21 21:41:34 +0800

    add keil rtt

---
+++ 02_led_rtthread_mini_system_keil/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'first' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32h7xx.h"
+
+#define RTE_DEVICE_STARTUP_STM32H7XX    /* Device Startup for STM32H7 */
+
+#endif /* RTE_COMPONENTS_H */
---
+++ 02_led_rtthread_mini_system_keil/main.c
@@ -0,0 +1,26 @@
+#include "stm32h7xx.h"
+#include "rtthread.h"
+#define GPIO_PIN_8                 ((uint16_t)0x0100)  /* Pin 8 selected    */
+struct rt_semaphore dynamic_sem;
+
+int main(void)
+{
+    int i;
+    rt_sem_init(&dynamic_sem, "dsem", 0, RT_IPC_FLAG_FIFO);
+    static rt_err_t result;
+    RCC->AHB4ENR |= RCC_AHB4ENR_GPIOIEN;
+    GPIOI->MODER = 0xFFFDFFFF;
+    while(1)
+    {
+        result = rt_sem_take(&dynamic_sem, RT_WAITING_FOREVER);
+        if(i%2 == 0)
+        {
+            GPIOI->BSRR = GPIO_PIN_8;
+        }
+        else
+        {
+            GPIOI->BSRR = GPIO_PIN_8 << 16;
+        }
+        i++;
+    }
+}
